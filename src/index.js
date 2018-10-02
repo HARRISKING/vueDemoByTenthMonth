9f09f4593e5463c83f012beddcec056cf95888ca
HARRISKING <llc13588440410@outlook.com>
2018-10-02 10:21:17 +0800

    新的修改

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+var app = new Vue({
+    el:'#app',
+    data:{
+        message:'1111',
+        lists:[
+        {content:'123'},
+            {content:'123hh'},
+            {content:'123fgd'},
+            {content:'123ss'},
+            {content:'1fgfd'},
+
+        ],
+        isShow: true,
+    },
+    methods:{
+        clickMe: function(){
+            if( this.isShow === true){
+                this.isShow = false
+            }else{
+                this.isShow = true
+            }
+            console.log(this.isShow)
+        }
+    }
+})
